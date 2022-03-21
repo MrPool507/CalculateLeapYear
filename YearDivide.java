e35aa6bcae03b886ae8375ba1d210187add56014
MrPool507 <93581111+MrPool507@users.noreply.github.com>
2022-03-21 14:04:42 +0000

    Update YearDivide.java

--- YearDivide.java
+++ YearDivide.java
@@ -6,23 +6,56 @@ class YearDivide
 {
  // Function to find that number
  // is divisible by 4 or not
- static boolean check(String str)
- {
-     int n = str.length();
 
-     // Empty string
-     if (n == 0)
-         return false;
 
-     // If there is single digit
-     if (n == 1)
-         return ((str.charAt(0) - '0') % 4 == 0);
+    // Class describes the effects of an earthquake
+        // (To demonstrate use of Code Coverage Tool)
 
-     // If number formed by last two digits is
-     // divisible by 4.
-     int last = str.charAt(n - 1) - '0';
-     int second_last = str.charAt(n - 2) - '0';
-     return ((second_last * 10 + last) % 4 == 0);
- 
- }
- }
+        // Attributes
+        private int year;
+
+
+        public void year(int a) {
+
+            year = a;
+        }
+
+        // Method
+        // Gets and returns a description of the effects of the earthquake
+
+        static boolean check(String str)
+        {
+            int n = str.length();
+     
+            // Empty string
+            if (n == 0)
+                return false;
+     
+            // If there is single digit
+            if (n == 1)
+                return ((str.charAt(0) - '0') % 4 == 0);
+     
+            // If number formed by last two digits is
+            // divisible by 4.
+            int last = str.charAt(n - 1) - '0';
+            int second_last = str.charAt(n - 2) - '0';
+            return ((second_last * 10 + last) % 4 == 0);
+        }
+        public String getDescription()
+        {
+
+            String leapyear;
+            try {
+                if (year >= 1582 && year<= 10000)
+                    leapyear = "invalid year";
+               
+            }
+            catch(Exception e)
+            {
+                leapyear=e.getMessage();
+            }
+
+            return leapyear;
+
+        }
+}
